44c193505333c59d5e1faa932027e667d883feaa
anismubarokah21 <anismubarokah21@gmail.com>
2023-12-29 08:55:52 +0700

    first commit

---
+++ server/model/model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    name : {
+        type : String,
+        required: true
+    },
+    alamat:{
+        type: String,
+        required:true
+    },
+    hobi:{
+        type: String,
+        required: true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender : String,
+    status : String
+})
+
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
